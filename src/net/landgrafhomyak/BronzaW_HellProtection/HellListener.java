83f4ae5122179474673771c32f09992d1d71efc8
Andrew <tankist.scratch.p@gmail.com>
2021-09-18 18:11:50 +0300

    Teleport player from space under the bedrock layer on all worlds

--- src/net/landgrafhomyak/BronzaW_HellProtection/HellListener.java
+++ src/net/landgrafhomyak/BronzaW_HellProtection/HellListener.java
@@ -13,16 +13,18 @@ import static org.bukkit.Bukkit.getServer;
 
 public class HellListener implements Listener {
     private void message(String name) {
-        getServer().broadcastMessage(String.format("'%s' teleported to spawn from hell's roof", name));
+        getServer().broadcastMessage(String.format("'%s' reached the border of the world and miraculously found a spawn behind it", name));
     }
 
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
         if (!event.getPlayer().isOp()) {
-            if (event.getTo() != null && event.getTo().getWorld().getEnvironment() == World.Environment.NETHER && event.getTo().getY() >= 127) {
-                this.message(event.getPlayer().getName());
-                event.setCancelled(true);
-                event.getPlayer().teleport(getServer().getWorlds().get(0).getSpawnLocation());
+            if (event.getTo() != null) {
+                if (event.getTo().getWorld().getEnvironment() == World.Environment.NETHER && event.getTo().getY() >= 127 || event.getTo().getY() <= 0) {
+                    this.message(event.getPlayer().getName());
+                    event.setCancelled(true);
+                    event.getPlayer().teleport(getServer().getWorlds().get(0).getSpawnLocation());
+                }
             }
         }
     }
@@ -31,10 +33,12 @@ public class HellListener implements Listener {
     @EventHandler
     public void onTeleport(EntityTeleportEvent event) {
         if (event.getEntity().getType() == EntityType.PLAYER && !((Player) event.getEntity()).isOp()) {
-            if (event.getTo() != null && event.getTo().getWorld().getEnvironment() == World.Environment.NETHER && event.getTo().getY() >= 127) {
-                this.message(event.getEntity().getName());
-                event.setCancelled(true);
-                event.getEntity().teleport(getServer().getWorlds().get(0).getSpawnLocation());
+            if (event.getTo() != null) {
+                if (event.getTo().getWorld().getEnvironment() == World.Environment.NETHER && event.getTo().getY() >= 127 || event.getTo().getY() <= 0) {
+                    this.message(event.getEntity().getName());
+                    event.setCancelled(true);
+                    event.getEntity().teleport(getServer().getWorlds().get(0).getSpawnLocation());
+                }
             }
         }
     }
@@ -42,10 +46,12 @@ public class HellListener implements Listener {
     @EventHandler
     public void onPlayerTeleport(PlayerTeleportEvent event) {
         if (!event.getPlayer().isOp()) {
-            if (event.getTo() != null && event.getTo().getWorld().getEnvironment() == World.Environment.NETHER && event.getTo().getY() >= 127) {
-                this.message(event.getPlayer().getName());
-                event.setCancelled(true);
-                event.getPlayer().teleport(getServer().getWorlds().get(0).getSpawnLocation());
+            if (event.getTo() != null) {
+                if (event.getTo().getWorld().getEnvironment() == World.Environment.NETHER && event.getTo().getY() >= 127 || event.getTo().getY() <= 0) {
+                    this.message(event.getPlayer().getName());
+                    event.setCancelled(true);
+                    event.getPlayer().teleport(getServer().getWorlds().get(0).getSpawnLocation());
+                }
             }
         }
     }
